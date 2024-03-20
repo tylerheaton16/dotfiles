4c3a58e5c6511430a349e32d1a536dd9444d9955
Tyler <tylerheaton16@gmail.com>
2024-03-20 14:56:35 -0400

    updates

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -63,7 +63,8 @@
 
         -- Custom Parameters (with defaults)
             opts = {
-                model = "mistral", -- The default model to use.
+                --model = "mistral", -- The default model to use.
+                model = "codellama", -- The default model to use.
                 host = "localhost", -- The host running the Ollama service.
                 port = "11434", -- The port on which the Ollama service is listening.
                 display_mode = "split", -- The display mode. Can be "float" or "split".
